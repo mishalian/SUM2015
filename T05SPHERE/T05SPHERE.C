662e2b2955ed37a2961381cfbc9f553470b6f371
mishalian <misha.cherk@gmail.com>
2015-06-05 18:15:05 +0400

    Uploading

---
+++ T05SPHERE/T05SPHERE.C
@@ -0,0 +1,280 @@
+/*T05SPHERE
+ *MC6 spr11
+ *05.06.2015
+ *Build a sphere in a three-dimentional space.
+ */
+
+/*T02CLOCK.C
+ *MC11
+ *02.06.2015
+ *Create clock animation.
+ */
+
+#pragma warning(disable: 4244)
+
+#include <math.h>
+#include <string.h>
+#include <stdlib.h>
+#include <time.h>
+
+#include <windows.h>
+
+#define PI 3.14159265358979323846
+
+#define WND_CLASS_NAME "My window class"
+#define N 10
+#define M 10
+
+typedef struct
+{
+  DOUBLE X, Y, Z;
+} VEC;
+
+VEC Dot[M][N];
+
+LRESULT CALLBACK MyWindowFunc( HWND hWnd, UINT Msg,
+                               WPARAM wParam, LPARAM lParam ); /* Функция обработки. */
+
+/* Главная функция программы.
+ *   - дескриптор экземпляра приложения:
+ *       HINSTANCE hInstance;
+ *   - дескриптор предыдущего экземпляра приложения
+ *     (не используется и должно быть NULL):
+ *       HINSTANCE hPrevInstance;
+ *   - командная строка:
+ *       CHAR *CmdLine;
+ * ВОЗВРАЩАЕМОЕ ЗНАЧЕНИЕ:
+ *   (INT) код возврата в операционную систему.
+ *   0 - при успехе.
+ */
+INT WINAPI WinMain( HINSTANCE hInstance, HINSTANCE hPrevInstance,
+                    CHAR *CmdLine, INT ShowCmd )
+{
+  WNDCLASS wc;
+  HWND hWnd;
+  MSG msg;
+
+  wc.style = CS_VREDRAW | CS_HREDRAW; /* Стиль окна */
+  wc.cbClsExtra = 0;
+  wc.cbWndExtra = 0;
+  wc.hbrBackground = CreateSolidBrush(RGB(0, 0, 0));
+  wc.hCursor = LoadCursor(NULL, IDC_HAND); /* Загрузка курсора (системного) */
+  wc.hIcon = LoadIcon(NULL, IDI_ASTERISK); /* Загрузка пиктограммы (системной) */
+  wc.hInstance = hInstance; /* Дескриптор приложения, регистрирующего класс */
+  wc.lpszMenuName = NULL; /* Имя ресурса меню */
+  wc.lpfnWndProc = MyWindowFunc; /* Указатель на функцию обработки */
+  wc.lpszClassName = WND_CLASS_NAME;
+
+
+  /* Регистрация класса в системе */
+  if (!RegisterClass(&wc))
+  {
+    MessageBox(NULL, "Error register window class", "ERROR", MB_OK);
+    return 0;
+  }
+
+    /* Создание окна */
+  hWnd =
+    CreateWindow(WND_CLASS_NAME,    /* Имя класса окна */
+      "3D Sphere",                      /* Заголовок окна */
+      WS_OVERLAPPEDWINDOW | WS_CLIPCHILDREN,          /* Стили окна - окно общего вида */
+      960 - 500, 540 - 500, /* Позиция окна (x, y) - по умолчанию */
+      1000, 1000, /* Размеры окна (w, h) - по умолчанию */
+      NULL,                         /* Дескриптор родительского окна */
+      NULL,                         /* Дескриптор загруженного меню */
+      hInstance,                    /* Дескриптор приложения */
+      NULL);                        /* Указатель на дополнительные параметры */
+
+  ShowWindow(hWnd, ShowCmd);
+  UpdateWindow(hWnd);
+
+    /* Запуск цикла сообщений окна */
+  while (GetMessage(&msg, NULL, 0, 0))
+  {
+    /* Обработка сообщений от клавиатуры */
+    TranslateMessage(&msg);
+    /* Передача сообщений в функцию окна */
+    DispatchMessage(&msg);
+  }
+  return msg.wParam;
+}
+
+
+VEC RotateX( VEC P, DOUBLE AngleDegree )
+{
+  DOUBLE a = AngleDegree * PI / 180, si = sin(a), co = cos(a);
+  VEC r;
+
+  r.X = P.X;
+  r.Y = P.Y * co - P.Z * si;
+  r.Z = P.Y * si + P.Z * co;
+  return r;
+} /* End of 'RotateX' function */
+
+VEC VecCrossVec( VEC A, VEC B )
+{
+  VEC r;
+
+  r.X = A.Y * B.Z - A.Z * B.Y;
+  r.Y = A.Z * B.X - A.X * B.Z;
+  r.Z = A.X * B.Y - A.Y * B.X;
+  return r;
+} /* End of 'VecCrossVec' function */
+
+/* Функция вычитания векторов */
+VEC VecSubVec( VEC A, VEC B )
+{
+  VEC r;
+
+  r.X = A.X - B.X;
+  r.Y = A.Y - B.Y;
+  r.Z = A.Z - B.Z;
+  return r;
+} /* End of 'VecSubVec' function */
+
+/* Рисование четырехугольника */
+VOID DrawQuad( HDC hDC, VEC P0, VEC P1, VEC P2, VEC P3, INT W, INT H )
+{
+  VEC Norm = VecCrossVec(VecSubVec(P3, P0), VecSubVec(P1, P0));
+  POINT pnts[4];
+
+  /* back-face culling */
+  if (Norm.Z < 0)
+    return;
+
+  pnts[0].x = P0.X + W / 2;
+  pnts[0].y = -P0.Y + H / 2;
+
+  pnts[1].x = P1.X + W / 2;
+  pnts[1].y = -P1.Y + H / 2;
+
+  pnts[2].x = P2.X + W / 2;
+  pnts[2].y = -P2.Y + H / 2;
+
+  pnts[3].x = P3.X + W / 2;
+  pnts[3].y = -P3.Y + H / 2;
+
+  Polygon(hDC, pnts, 4);
+} /* End of 'DrawQuad' function */
+
+VOID DrawGlobe(HDC hDC, INT w, INT h)
+{
+  DOUBLE phi, theta, R = 200/*, t = clock() / (DOUBLE)CLOCK_PER_SEC*/;
+  INT i, j, x, y, z;
+
+  SelectObject(hDC, GetStockObject(NULL_PEN));
+  SelectObject(hDC, GetStockObject(DC_BRUSH));
+  SetDCBrushColor(hDC, RGB(0, 0, 0));
+  x = w / 2;
+  y = h / 2;
+  Ellipse(hDC, x - 2, y - 2, x + 2, y + 2);
+
+  /*for (i = 0; i < N; i++)
+  {
+    phi = i / (N - 1) * 2 * PI;
+    x = R * sin(phi) + w / 2;   
+    y = h / 2;
+
+    SelectObject(hDC, GetStockObject(NULL_PEN));
+    SelectObject(hDC, GetStockObject(DC_BRUSH));
+    SetDCBrushColor(hDC, RGB(0, 0, 0));
+    Ellipse(hDC, x - 2, y - 2, x + 2, y + 2);
+  } */
+}
+
+
+
+
+/* Функция обработки сообщения окна.
+ * АРГУМЕНТЫ:
+ *   - дескриптор окна:
+ *       HWND hWnd;
+ *   - номер сообщения (см. WM_***):
+ *       UINT Msg;
+ *   - параметр сообшения ('word parameter'):
+ *       WPARAM wParam;
+ *   - параметр сообшения ('long parameter'):
+ *       LPARAM lParam;
+ * ВОЗВРАЩАЕМОЕ ЗНАЧЕНИЕ:
+ *   (LRESULT) - в зависимости от сообщения.
+ */
+LRESULT CALLBACK MyWindowFunc( HWND hWnd, UINT Msg,
+                               WPARAM wParam, LPARAM lParam )
+{
+  CREATESTRUCT *cs; 
+  POINT pt;
+  SYSTEMTIME st;
+  HPEN hPen;
+  DOUBLE Angle;
+  static HDC hDC, hMemDC;
+  static HBITMAP hBm;
+  static INT w, h, R;
+
+
+  switch (Msg)
+  {
+  case WM_CREATE:
+    cs = (CREATESTRUCT *)lParam;
+    SetTimer(hWnd, 111, 50, NULL);
+
+    hDC = GetDC(hWnd);
+        /* создаем контекст в памяти */
+    hMemDC = CreateCompatibleDC(hDC);
+    hBm = CreateCompatibleBitmap(hDC, w, h);
+    ReleaseDC(hWnd, hDC);
+
+    return 0;
+
+  case WM_SIZE:
+    w = LOWORD(lParam);
+    h = HIWORD(lParam);
+
+    if (hBm != NULL)
+      DeleteObject(hBm);
+
+    hDC = GetDC(hWnd);
+    hBm = CreateCompatibleBitmap(hDC, w, h);
+    ReleaseDC(hWnd, hDC);
+
+    SelectObject(hMemDC, hBm);
+
+    SendMessage(hWnd, WM_TIMER, 111, 0);
+    return 0;
+
+  case WM_TIMER:
+    /* Clear Background */
+    SelectObject(hMemDC, GetStockObject(NULL_PEN));
+    SelectObject(hMemDC, GetStockObject(DC_BRUSH));
+    SetDCBrushColor(hMemDC, RGB(255, 255, 150));
+    Rectangle(hMemDC, 0, 0, w + 1, h + 1);
+    
+
+    GetLocalTime(&st);
+
+/*    SelectObject(hMemDC, GetStockObject(NULL_PEN));
+    SelectObject(hMemDC, GetStockObject(DC_BRUSH));
+    SetDCBrushColor(hMemDC, RGB(0, 0, 0));*/
+    DrawGlobe(hDC, w, h);
+    SelectObject(hMemDC, hBm);
+
+    BitBlt(hDC, 0, 0, w, h, hMemDC, 0, 0, SRCCOPY);
+  
+    InvalidateRect(hWnd, NULL, TRUE);
+    return 0;
+
+  case WM_CLOSE:
+    break;
+
+  case WM_ERASEBKGND:
+    BitBlt(hDC, 0, 0, w, h, hMemDC, 0, 0, SRCCOPY);
+    return 0;
+
+  case WM_DESTROY:
+    DeleteDC(hMemDC);
+    KillTimer(hWnd, 111);
+    PostQuitMessage(0);
+    return 0;
+  }
+  return DefWindowProc(hWnd, Msg, wParam, lParam);
+} /* End of 'MyWindowFunc' function */
+
